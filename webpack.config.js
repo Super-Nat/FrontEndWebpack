10afe3093f7b8cdf77c4dd0f812bb299abb2b87b
Nat-osc <83262215+Nat-osc@users.noreply.github.com>
2022-08-05 17:50:01 +0700

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,119 +1,117 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const cssnano = require("cssnano");
-const UgliFyJsPlugin = require('uglifyjs-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UgliFyJsPlugin = require("uglifyjs-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
-const webpack = require('webpack');
+const webpack = require("webpack");
 
-const JS_DIR = path.resolve(__dirname,'src/js');
-const IMG_DIR = path.resolve(__dirname,'src/images');
-const BUILD_DIR = path.resolve(__dirname,'build');
+const JS_DIR = path.resolve(__dirname, "src/js");
+const IMG_DIR = path.resolve(__dirname, "src/images");
+const BUILD_DIR = path.resolve(__dirname, "build");
 
 const entry = {
-    main : JS_DIR + '/main.js'
+	main: JS_DIR + "/main.js",
 };
 
 const output = {
-    path: BUILD_DIR,
-    filename: 'js/[name].js'
+	path: BUILD_DIR,
+	filename: "js/[name].js",
 };
 
-
 const rules = [
-    {
-        test: /\.js$/,
-        include: [JS_DIR],
-        exclude: /node_modules/,
-        use:{
-            loader:'babel-loader',
-            options:{
-                presets:['@babel/preset-env'],
-                plugins:['@babel/plugin-proposal-class-properties']
-            }
-        }
-    },
-    {
-        test: /\.s[ac]ss$/i,
-        exclude: /node_modules/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader','sass-loader']
-    },
-    {
-        test: /\.(png|jpg|svg|jpeg|gif|ico)$/,
-        use: [
-            {
-                loader: 'file-loader',
-                options: {
-                    name:' [path][name].[ext]',
-                    // publicPath: 'production' === process.env.NODE_ENV ? '../' : '../../'
-                }
-            }
-        ]
-    },
-    {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/'
-            }
-          }
-        ]
-      }
+	{
+		test: /\.js$/,
+		include: [JS_DIR],
+		exclude: /node_modules/,
+		use: {
+			loader: "babel-loader",
+			options: {
+				presets: ["@babel/preset-env"],
+				plugins: ["@babel/plugin-proposal-class-properties"],
+			},
+		},
+	},
+	{
+		test: /\.s[ac]ss$/i,
+		exclude: /node_modules/,
+		use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+	},
+	{
+		test: /\.(png|jpg|svg|jpeg|gif|ico)$/,
+		exclude: /fonts/,
+		use: [
+			{
+				loader: "file-loader",
+				options: {
+					name: "[name].[ext]",
+					publicPath: "../images",
+					outputPath: "images/",
+				},
+			},
+		],
+	},
+	{
+		test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+		use: [
+			{
+				loader: "file-loader",
+				options: {
+					name: "[name].[ext]",
+					outputPath: "fonts/",
+				},
+			},
+		],
+	},
 ];
 
 const plugins = (argv) => {
+	return [
+		new CleanWebpackPlugin({
+			cleanStaleWebpackAssets: "production" === argv.mode,
+		}),
+		new MiniCssExtractPlugin({
+			filename: "css/[name].css",
+		}),
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: "src/html/index.html",
+			minify: false,
+		}),
+		new CopyPlugin({
+			patterns: [{ from: "src/images", to: "images/" }],
+		}),
+		new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery",
+		}),
+	];
+};
 
-    return [
-        new CleanWebpackPlugin({
-            cleanStaleWebpackAssets: ('production' === argv.mode)
-        }),
-        new MiniCssExtractPlugin({
-            filename : 'css/[name].css'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'src/html/index.html',
-            minify: false
-        }),
-        new CopyPlugin({
-            patterns: [
-              { from: "src/images", to: "images/" },
-            ],
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-        })
-    ]
-}
-
-module.exports = (env,argv) => ({
-    entry: entry,
-    output: output,
-    target: "web",
-    devtool: 'source-map',
-    plugins: plugins(argv),
-    module: {
-        rules: rules
-    },
-    optimization:{
-        minimizer:[
-            new OptimizeCssAssetsPlugin({
-                cssProcessor:cssnano
-            }),
-            new UgliFyJsPlugin({
-                cache:false,
-                parallel:true,
-                sourceMap:false
-            })
-        ]
-    },
-    externals:{
-        jquery:'jQuery'
-    }
+module.exports = (env, argv) => ({
+	entry: entry,
+	output: output,
+	target: "web",
+	devtool: "source-map",
+	plugins: plugins(argv),
+	module: {
+		rules: rules,
+	},
+	optimization: {
+		minimizer: [
+			new OptimizeCssAssetsPlugin({
+				cssProcessor: cssnano,
+			}),
+			new UgliFyJsPlugin({
+				cache: false,
+				parallel: true,
+				sourceMap: false,
+			}),
+		],
+	},
+	externals: {
+		jquery: "jQuery",
+	},
 });
